a4230edc174c7e7fbc52acbf2df5e57ca3a569b0
Walter Chiwo <waltchiwo@gmail.com>
2022-12-24 18:08:02 -0600

    Initial Commit

---
+++ Moms MusicTests/Moms_MusicTests.swift
@@ -0,0 +1,36 @@
+//
+//  Moms_MusicTests.swift
+//  Moms MusicTests
+//
+//  Created by Walter Chiwo on 12/24/22.
+//  Copyright © 2022 Walter Chiwo. All rights reserved.
+//
+
+import XCTest
+@testable import Moms_Music
+
+class Moms_MusicTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
